460734deed474a566cd16146b38298358fe9f9bf
Diksha Rajput <diksharajput1703@gmail.com>
2024-10-19 14:18:21 +0530

    Implementation: support custom delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,7 +3,13 @@ function add(numbers) {
         return 0;
     }
     
-    const numArray = numbers.split(/[\n,]/);
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split('\n');
+        delimiter = new RegExp(`[${parts[0].charAt(2)}]`);
+        numbers = parts[1];
+    }
+    const numArray = numbers.split(delimiter);
     return numArray.reduce((sum, current) => sum + parseInt(current), 0);
 }
 
