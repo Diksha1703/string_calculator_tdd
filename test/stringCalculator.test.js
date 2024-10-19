222395dacc221a9fa4c06c4abe391c18f6e18144
Diksha Rajput <diksharajput1703@gmail.com>
2024-10-19 13:59:43 +0530

    Test: return sum of two numbers

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -7,3 +7,7 @@ test('should return 0 for an empty string', () => {
 test('should return number itself for a single number string', () => {
     expect(add("1")).toBe(1);
 });
+
+test('should return sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+});
